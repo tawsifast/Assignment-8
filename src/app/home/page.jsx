a3760fdeee4ed8cf9e226c1a51f26e0c3c4d4071
tawsifast <0070tawsif@gmail.com>
2026-04-29 21:20:08 +0600

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className='text-5xl'>Homepage</h2>
+        </div>
+    );
+};
+
+export default HomePage;
