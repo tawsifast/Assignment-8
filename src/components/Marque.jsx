1bbd0d639148bfaea75b18c05887acc0256fb6cb
tawsifast <0070tawsif@gmail.com>
2026-05-04 20:11:51 +0600

    new product added in data.json

--- src/components/Marque.jsx
+++ src/components/Marque.jsx
@@ -6,9 +6,9 @@ import { Button } from '@heroui/react';
 const Marque = async () => {
     const products = await getProduct();
     return (
-        <div className='flex justify-between items-center gap-4 p-2 bg-gray-300'>
-            <Button className={"rounded-sm"}>Brands</Button>
-            <Marquee>
+        <div className='flex justify-between bg-gray-300 items-center gap-4 p-2 '>
+            <Button className={"rounded-sm bg-[#1A1A2E]/90 text-white"}>Brands</Button>
+            <Marquee pauseOnHover={true}>
               {
                 products.map((product)=>
                 <span key={product.id} className='mx-20'>{product.brand}</span>)
